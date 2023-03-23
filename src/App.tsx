686540522bf0f5671c398d22f333392084b5b3d7
Maxim <76053782+ggodos@users.noreply.github.com>
2023-03-23 11:15:15 +0500

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      Hello
+      <canvas id='mainCanvas'>Your browser doesn't support HTML5 Canvas</canvas>
+    </div>
+  )
+}
+
+export default App
