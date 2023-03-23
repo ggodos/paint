80ef287e44704a4a37c2b30158380044eaac2a5b
Maxim <76053782+ggodos@users.noreply.github.com>
2023-03-23 11:29:39 +0500

    aoaoao

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <div className="App">
-      Hello
+      Hello again
       <canvas id='mainCanvas'>Your browser doesn't support HTML5 Canvas</canvas>
     </div>
   )
